44e3977d03ad5998137ff110e1edf3e7870415eb
Luciana Maroun <luciana@paqueta.lbd.dcc.ufmg.br>
2015-08-18 10:47:50 -0300

    MF baseline implementation for reviews

---
+++ betf/mf.py
@@ -0,0 +1,47 @@
+from numpy import nan
+from numpy.random import random
+
+K = 2
+
+_ITER = 1000    # number of iterations of stochastic gradient descent
+_ALPHA = 0.01   # learning rate
+_BETA = 0.01    # regularization factor
+
+
+class MF_Model(object):
+
+  def __init__(self):
+    self.U = None # Matrix of user (voter( latent arrays (N_v, K)
+    self.R = None # Matrix of review latent arrays (N_r, K)
+    self.user_map = None # Map from user ids to matrix indices (lines)
+    self.review_map = None # Map from review ids to matrix indices (lines)
+
+  def initialize_matrices(self, votes):
+    users = sorted(set([vote['voter'] for vote in votes]))
+    reviews = sorted(set([vote['review'] for vote in votes]))
+    self.user_map = {u:i for i, u in enumerate(users)}
+    self.review_map = {r:i for i, r in enumerate(reviews)}
+    self.U = random((len(users), K))
+    self.R = random((len(reviews), K))
+
+  def fit(self, votes):
+    self.initialize_matrices(votes)
+    for _ in xrange(_ITER):
+      for vote in votes:
+        u = self.user_map[vote['voter']]
+        r = self.review_map[vote['review']]
+        error = vote['vote'] - self.U[u,:].dot(self.R[r,:].T)
+        for i in xrange(K):
+          self.U[u,i] += _ALPHA * (2 * error * self.R[r,i] - _BETA * self.U[u,i])
+          self.R[r,i] += _ALPHA * (2 * error * self.U[u,i] - _BETA * self.R[r,i])
+  
+  def predict(self, votes):
+    pred = []
+    for vote in votes:
+      u = self.user_map[vote['voter']] if vote['voter'] in user_map else -1
+      r = self.review_map[vote['review']] if vote['review'] in user_map else -1
+      if u != -1 and r != -1:
+        pred.append(U[u,:].dot(R[r,:].T)[0,0])
+      else:
+        pred.append(nan)
+
